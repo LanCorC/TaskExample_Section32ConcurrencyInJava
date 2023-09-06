13ea3beebda2ba208b782c5852f6151a8a6f523e
LanCorC <lanc3.corcega@gmail.com>
2023-09-07 03:01:14 +1200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.taskexample_section32concurrencyinjava {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example.taskexample_section32concurrencyinjava to javafx.fxml;
+    exports com.example.taskexample_section32concurrencyinjava;
+}
