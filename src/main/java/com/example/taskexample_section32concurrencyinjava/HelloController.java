cb7e8660500de913dfcba68fb738c42e09522782
LanCorC <lanc3.corcega@gmail.com>
2023-09-07 03:04:39 +1200

    Section 23.412(Concurrency in Java: JavaFX Background Tasks): setting up code for demonstration; introduced to Platform.runLater() and how to bind ObservableObject to ListView via ask Task instance's .valueProperty(); Task as means to access background working thread to keep separate from UI thread

--- src/main/java/com/example/taskexample_section32concurrencyinjava/HelloController.java
+++ src/main/java/com/example/taskexample_section32concurrencyinjava/HelloController.java
@@ -1,14 +1,45 @@
 package com.example.taskexample_section32concurrencyinjava;
 
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private ListView listView;
+
+    private Task<ObservableList<String>> task;
+
+    public void initialize() {
+        task = new Task<>() {
+            @Override
+            protected ObservableList<String> call() throws Exception {
+                Thread.sleep(1000);
+
+                ObservableList<String> employees = FXCollections.observableArrayList(                        "Tim Buchalka",
+                        "Bill Rogers",
+                        "Jack Jill",
+                        "Joan Andrews",
+                        "Mary Johnson",
+                        "Bob McDonald");
+
+                return employees;
+            }
+        };
+
+        listView.itemsProperty().bind(task.valueProperty());
+
+    }
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void buttonPressed() {
+        new Thread(task).start();
     }
+
 }
